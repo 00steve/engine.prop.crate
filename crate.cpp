c97526b7c976bbf4faaf1552784325e7adeb9e9d
00steve <stevemkrenz@gmail.com>
2016-10-30 01:35:40 -0500

    Crate now requests that physics objects be created and returned to it.

--- crate.cpp
+++ crate.cpp
@@ -1,41 +1,110 @@
+
 #include "crate.h"
+
+string Crate::geometry = "box";
+List<double> Crate::lengths = List<double>(3,new double[3]{1,1,1});
+//Crate::lengths.Push(1);
+//Crate::lengths.Push(1);
+//Crate::lengths.Push(1);
+double Crate::density = 1;
 
 
 Crate::Crate(){
+    //PhysicsWorld::NewBox(body,geom,double3(1,1,1),1,this);
 
 }
 
-Crate::~Crate()
-{
-    //dtor
+Crate::~Crate(){
+
 }
 
+bool Crate::HandleMessage(NodeMessage message){
+    cout << "Crate::HandleMessage - " << message.code << endl;
+    switch(message.code){
+    case MESSAGE_REGISTER_PHYSICS_OFFER: {
+            VarMap *physics = new VarMap();
+            physics->Add<string>(&geometry,"crate.geometry");
+            physics->Add<List<double> >(&lengths,"crate.lengths");
+            physics->Add<double>(&density,"crate.density");
+            physics->Add<double3>(&position,"crate.position");
+            cout << "position : " << position.x << "," << position.y << "," << position.z << endl;
+            CreateAndSendMessage(message.sender,MESSAGE_REGISTER_PHYSICS_REQUEST,(void*)physics);
+            return true;
+        }
 
-void Crate::OnSetSettings(){
-    PhysicsWorld::NewBox(body,geom,double3(1,1,1),1,(void*)this);
+    case MESSAGE_REGISTER_PHYSICS_FINISHED: {
+            cout << "successfully created physics object\n";
+            PhysicsGroup *pGroup = (PhysicsGroup*) message.data;
+            body = pGroup->body["crate"];
+            geom = pGroup->geom["crate"];
+            return true;
+        }
+    }
+    return PhysicalSingleBody::HandleMessage(message);
+}
+
+void Crate::Update(){
+    PhysicalSingleBody::Update();
 }
 
 void Crate::Draw(){
-    glColor3f(1,1,0);
-    glBegin(GL_TRIANGLE_FAN);
 
-        double3 d = double3(.5,.5,.5);
+        //cout << " draw crate\n";
+        glPushMatrix();
+        //glTranslated(cratePosition.x,cratePosition.y,cratePosition.z);
+        glMultMatrixf(gl_matrix);
 
-        glColor3f(d.x*-1,d.y*-1,d.z*-1);
-        glVertex3d(d.x*-.5,d.y*-.5,d.z*-.5);
-        glColor3f(d.x*-1,d.y,d.z*-1);
-        glVertex3d(d.x*-.5,d.y*.5,d.z*-.5);
-        glColor3f(d.x,d.y,d.z*-1);
-        glVertex3d(d.x*.5,d.y*.5,d.z*-.5);
-        glColor3f(d.x,d.y*-1,d.z*-1);
-        glVertex3d(d.x*.5,d.y*-.5,d.z*-.5);
-        glColor3f(d.x,d.y*-1,d.z);
-        glVertex3d(d.x*.5,d.y*-.5,d.z*.5);
-        glColor3f(d.x*-1,d.y*-1,d.z);
-        glVertex3d(d.x*-.5,d.y*-.5,d.z*.5);
-        glColor3f(d.x*-1,d.y,d.z);
-        glVertex3d(d.x*-.5,d.y*.5,d.z*.5);
-        glColor3f(d.x*-1,d.y,d.z*-1);
-        glVertex3d(d.x*-.5,d.y*.5,d.z*-.5);
-    glEnd();
+        double3 d = double3(1,1,1);
+        glBegin(GL_TRIANGLE_FAN);
+
+
+			glColor3f(d.x,d.y,d.z);
+			glVertex3d(d.x*.5,d.y*.5,d.z*.5);
+            glColor3f(d.x,d.y*-1,d.z);
+			glVertex3d(d.x*.5,d.y*-.5,d.z*.5);
+
+
+
+            glColor3f(d.x,d.y*-1,d.z*-1);
+			glVertex3d(d.x*.5,d.y*-.5,d.z*-.5);
+            glColor3f(d.x,d.y,d.z*-1);
+			glVertex3d(d.x*.5,d.y*.5,d.z*-.5);
+            glColor3f(d.x*-1,d.y,d.z*-1);
+			glVertex3d(d.x*-.5,d.y*.5,d.z*-.5);
+            glColor3f(d.x*-1,d.y,d.z);
+			glVertex3d(d.x*-.5,d.y*.5,d.z*.5);
+            glColor3f(d.x*-1,d.y*-1,d.z);
+			glVertex3d(d.x*-.5,d.y*-.5,d.z*.5);
+
+
+
+
+            glColor3f(d.x,d.y*-1,d.z);
+			glVertex3d(d.x*.5,d.y*-.5,d.z*.5);
+		glEnd();
+        glColor3f(1,1,0);
+		glBegin(GL_TRIANGLE_FAN);
+
+
+            glColor3f(d.x*-1,d.y*-1,d.z*-1);
+			glVertex3d(d.x*-.5,d.y*-.5,d.z*-.5);
+            glColor3f(d.x*-1,d.y,d.z*-1);
+			glVertex3d(d.x*-.5,d.y*.5,d.z*-.5);
+            glColor3f(d.x,d.y,d.z*-1);
+			glVertex3d(d.x*.5,d.y*.5,d.z*-.5);
+            glColor3f(d.x,d.y*-1,d.z*-1);
+			glVertex3d(d.x*.5,d.y*-.5,d.z*-.5);
+            glColor3f(d.x,d.y*-1,d.z);
+			glVertex3d(d.x*.5,d.y*-.5,d.z*.5);
+            glColor3f(d.x*-1,d.y*-1,d.z);
+			glVertex3d(d.x*-.5,d.y*-.5,d.z*.5);
+            glColor3f(d.x*-1,d.y,d.z);
+			glVertex3d(d.x*-.5,d.y*.5,d.z*.5);
+            glColor3f(d.x*-1,d.y,d.z*-1);
+			glVertex3d(d.x*-.5,d.y*.5,d.z*-.5);
+
+		glEnd();
+
+    glPopMatrix();
+
 }
--- crate.h
+++ crate.h
@@ -1,21 +1,31 @@
 #ifndef CRATE_H
 #define CRATE_H
 
-
 #include <engine/physics/physical_single_body.h>
+#include <engine/physics/physics_world.h>
+#include <GL/gl.h>
+#define dSINGLE
+#include <ode/ode.h>
 
-class Crate : public PhysicalSingleBody {
+
+class Crate : public PhysicalSingleBody{
+private:
+
+    static string geometry;
+    static List<double> lengths;
+    static double density;
 
 protected:
 
-    virtual void OnSetSettings();
+    virtual bool HandleMessage(NodeMessage message);
 
 public:
 
     Crate();
     ~Crate();
 
-    virtual void Draw();
+    virtual void Update();
+    void Draw();
 
 };
 
